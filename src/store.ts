c39770f8742c1eca963866d6eabaee3797bd3d58
cgamcs <cesar.garciamcs@proton.me>
2026-01-11 13:42:09 -0600

    obteniendo llamada a la API con zustand

--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,14 @@
+import axios from "axios"
 import { create } from "zustand"
+
+async function getCryptos() {
+  const url = 'https://data-api.coindesk.com/asset/v1/top/list?page=1&page_size=10&sort_by=CIRCULATING_MKT_CAP_USD&sort_direction=DESC&groups=ID,BASIC,PRICE&toplist_quote_asset=USD'
+  const { data : { Data : { LIST } } } = await axios(url)
+  console.log(LIST)
+}
 
 export const useCryptoStore = create(() => ({
   fetchCryptos: () => {
-    console.log('desde fecthCrytos')
+    getCryptos()
   }
 }))

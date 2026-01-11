5db936f46005f19f021bfb455548bb2dedac88ac
cgamcs <cesar.garciamcs@proton.me>
2026-01-11 14:34:37 -0600

    moviendo la funcion que obtiene la API a la carpeta services

---
+++ src/services/CryptoService.ts
@@ -0,0 +1,12 @@
+import axios from "axios"
+import { CryptoCurrenciesResponseSchema } from "../schema/crypto-schema"
+
+export async function getCryptos() {
+  const url = 'https://data-api.coindesk.com/asset/v1/top/list?page=1&page_size=20&sort_by=CIRCULATING_MKT_CAP_USD&sort_direction=DESC&groups=ID,BASIC,PRICE&toplist_quote_asset=USD'
+  const { data : { Data : { LIST } } } = await axios(url)
+  const result = CryptoCurrenciesResponseSchema.safeParse(LIST)
+
+  if (result.success) {
+    return result.data
+  }
+}
--- src/store.ts
+++ src/store.ts
@@ -1,22 +1,12 @@
 import axios from "axios"
 import { create } from "zustand"
 import { devtools } from "zustand/middleware"
-import { CryptoCurrenciesResponseSchema } from "./schema/crypto-schema"
 import type { CryptoCurrency } from "./types"
+import { getCryptos } from './services/CryptoService'
 
 type CryptoStore = {
   cryptoCurrencies: CryptoCurrency
   fetchCryptos: () => Promise<void>
-}
-
-async function getCryptos() {
-  const url = 'https://data-api.coindesk.com/asset/v1/top/list?page=1&page_size=20&sort_by=CIRCULATING_MKT_CAP_USD&sort_direction=DESC&groups=ID,BASIC,PRICE&toplist_quote_asset=USD'
-  const { data : { Data : { LIST } } } = await axios(url)
-  const result = CryptoCurrenciesResponseSchema.safeParse(LIST)
-
-  if (result.success) {
-    return result.data
-  }
 }
 
 export const useCryptoStore = create<CryptoStore>()(devtools((set) => ({

d0deb76477c6ea35b82e933a7273ca9ac2d45a5c
cgamcs <cesar.garciamcs@proton.me>
2026-01-11 13:47:50 -0600

    validando la respuesta de la API con zod

--- src/schema/crypto-schema.ts
+++ src/schema/crypto-schema.ts
@@ -4,3 +4,10 @@ export const CurrencySchema = z.object({
   code: z.string(),
   name: z.string()
 })
+
+export const CryptoCurrencyResponseSchema = z.array(
+  z.object({
+    NAME: z.string(),
+    SYMBOL: z.string()
+  })
+)
--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,12 @@
 import axios from "axios"
 import { create } from "zustand"
+import { CryptoCurrencyResponseSchema } from "./schema/crypto-schema"
 
 async function getCryptos() {
-  const url = 'https://data-api.coindesk.com/asset/v1/top/list?page=1&page_size=10&sort_by=CIRCULATING_MKT_CAP_USD&sort_direction=DESC&groups=ID,BASIC,PRICE&toplist_quote_asset=USD'
+  const url = 'https://data-api.coindesk.com/asset/v1/top/list?page=1&page_size=20&sort_by=CIRCULATING_MKT_CAP_USD&sort_direction=DESC&groups=ID,BASIC,PRICE&toplist_quote_asset=USD'
   const { data : { Data : { LIST } } } = await axios(url)
-  console.log(LIST)
+  const result = CryptoCurrencyResponseSchema.safeParse(LIST)
+  console.log(result)
 }
 
 export const useCryptoStore = create(() => ({

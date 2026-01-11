dbc1c1a163350bdf41de7036ba1dc6b0e0b4efaf
cgamcs <cesar.garciamcs@proton.me>
2026-01-11 12:50:27 -0600

    creando formulario

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import CriptoSearchForm from "./components/CriptoSearchForm";
+
 function App() {
   return (
     <>
@@ -5,7 +7,7 @@ function App() {
         <h1 className="app-title">Cotizador de <span>Criptomonedas</span></h1>
 
         <div className="content">
-          
+          <CriptoSearchForm />
         </div>
       </div>
     </>
